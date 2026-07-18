9e49ad5c59430689b36efa90796f6fe5a92cccee
adarsh <adarshdeshmukh00000@gmail.com>
2026-07-19 03:59:50 +0530

    Refactor: change anchor tags to link for react router

--- Frontend/src/components/sidebar.jsx
+++ Frontend/src/components/sidebar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 function Sidebar()
 {
   return (
@@ -10,12 +11,12 @@ function Sidebar()
           </div>
         </div>
         <div className="flex flex-col gap-2 px-4">
-          <a href="/"className="py-3 pl-6 hover:bg-surface rounded-lg">Dashboard</a>
-          <a href="" className="py-3 pl-6 hover:bg-surface rounded-lg">Semester Details</a>
-          <a href="" className="py-3 pl-6 hover:bg-surface rounded-lg">AI study planner</a>
-          <a href="" className="py-3 pl-6 hover:bg-surface rounded-lg">Tasks and Planner</a>
-          <a href="" className="py-3 pl-6 hover:bg-surface rounded-lg">Attendence</a>
-          <a href="" className="py-3 pl-6 hover:bg-surface rounded-lg">Profile and Settings</a>
+          <link to='/' className="py-3 pl-6 hover:bg-surface rounded-lg">Dashboard</link>
+          <link to='/semester'  className="py-3 pl-6 hover:bg-surface rounded-lg">Semester Details</link>
+          <link to='/planner'  className="py-3 pl-6 hover:bg-surface rounded-lg">AI study planner</link>
+          <link to='/tasks'  className="py-3 pl-6 hover:bg-surface rounded-lg">Tasks and Planner</link>
+          <link to='/attendence'  className="py-3 pl-6 hover:bg-surface rounded-lg">Attendence</link>
+          <link to='/settinglink'  className="py-3 pl-6 hover:bg-surface rounded-lg">Profile and Settings</link>
         </div>
       </div>
   )
