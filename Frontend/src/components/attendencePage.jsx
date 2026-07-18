93c354569bc75a8aa7d7417da874318296c5aee0
adarsh <adarshdeshmukh00000@gmail.com>
2026-07-19 00:29:38 +0530

    add sample data to attendence page

--- Frontend/src/components/attendencePage.jsx
+++ Frontend/src/components/attendencePage.jsx
@@ -1,7 +1,63 @@
 function Attendence () {
+  const record = [
+  {
+    "id": 1,
+    "subject": "Data Structures",
+    "percentage": 71,
+    "attended_classes": 32,
+    "total_classes": 45,
+    "status_message": "Attend the next 7 classes to reach 75%",
+    "status_type": "danger"
+  },
+  {
+    "id": 2,
+    "subject": "DBMS",
+    "percentage": 89,
+    "attended_classes": 40,
+    "total_classes": 45,
+    "status_message": "You can skip up to 8 more classes and stay above 75%",
+    "status_type": "safe"
+  },
+  {
+    "id": 3,
+    "subject": "Operating Systems",
+    "percentage": 79,
+    "attended_classes": 34,
+    "total_classes": 43,
+    "status_message": "You can skip up to 2 more classes and stay above 75%",
+    "status_type": "safe"
+  },
+  {
+    "id": 4,
+    "subject": "Computer Networks",
+    "percentage": 93,
+    "attended_classes": 37,
+    "total_classes": 40,
+    "status_message": "You can skip up to 9 more classes and stay above 75%",
+    "status_type": "safe"
+  },
+  {
+    "id": 5,
+    "subject": "Software Engineering",
+    "percentage": 83,
+    "attended_classes": 30,
+    "total_classes": 36,
+    "status_message": "You can skip up to 4 more classes and stay above 75%",
+    "status_type": "safe"
+  },
+  {
+    "id": 6,
+    "subject": "Discrete Mathematics",
+    "percentage": 66,
+    "attended_classes": 23,
+    "total_classes": 35,
+    "status_message": "Attend the next 13 classes to reach 75%",
+    "status_type": "danger"
+  }
+]
   return(
     <div className="bg-bg relative min-h-screen text-heading">
-      
+
     </div>
   )
 }
