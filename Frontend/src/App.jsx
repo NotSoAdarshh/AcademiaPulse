d18be3d6296a89c01792ba751ceba56f664c3d1e
adarsh <adarshdeshmukh00000@gmail.com>
2026-07-18 16:18:37 +0530

    init basic structure- Semester details page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import LoginPage from './components/loginPage'
 import Sidebar from './components/sidebar'
 import Dashboard from './components/dashboardPage'
+import SemesterDetails from './components/semesterDetailsPage'
 
 function App() {
   
 
   return (
     <>
-    <Dashboard></Dashboard>
+    <SemesterDetails></SemesterDetails>
     </>
   )
 }
---
+++ Frontend/src/components/semesterDetailsPage.jsx
@@ -0,0 +1,75 @@
+function SemesterDetails () {
+
+  const courseData = [
+  {
+    subject: "Data Structures",
+    credits: 4,
+    attendance: 71,
+    score: 82,
+    grade: "A-",
+    syllabusCovered: 78
+  },
+  {
+    subject: "DBMS",
+    credits: 4,
+    attendance: 88,
+    score: 91,
+    grade: "A+",
+    syllabusCovered: 95
+  },
+  {
+    subject: "Operating Systems",
+    credits: 3,
+    attendance: 79,
+    score: 74,
+    grade: "B",
+    syllabusCovered: 60
+  },
+  {
+    subject: "Computer Networks",
+    credits: 3,
+    attendance: 92,
+    score: 88,
+    grade: "A",
+    syllabusCovered: 85
+  },
+  {
+    subject: "Software Engineering",
+    credits: 3,
+    attendance: 84,
+    score: 69,
+    grade: "B",
+    syllabusCovered: 70
+  },
+  {
+    subject: "Discrete Mathematics",
+    credits: 4,
+    attendance: 65,
+    score: 58,
+    grade: "C+",
+    syllabusCovered: 45
+  }
+];
+
+  return (
+    <div className="bg-bg min-h-screen text-heading">
+      <div className="bg-surface text-2xl font-bold py-4 pl-8">Semester Details</div>
+
+      <div className="flex gap-4 m-6">
+        <button type="button" className="p-2 px-4 bg-surface rounded-lg hover:bg-buttonClick">Sem 1</button>
+        <button type="button" className="p-2 px-4 bg-surface rounded-lg hover:bg-buttonClick">Sem 2</button>
+        <button type="button" className="p-2 px-4 bg-surface rounded-lg hover:bg-buttonClick">Sem 3</button>
+        <button type="button" className="p-2 px-4 bg-surface rounded-lg hover:bg-buttonClick">Sem 4</button>
+        <button type="button" className="p-2 px-4 bg-surface rounded-lg hover:bg-buttonClick">Sem 5</button>
+        <button type="button" className="p-2 px-4 bg-surface rounded-lg hover:bg-buttonClick">Sem 6</button>
+        <button type="button" className="p-2 px-4 bg-surface rounded-lg hover:bg-buttonClick">Sem 7</button>
+        <button type="button" className="p-2 px-4 bg-surface rounded-lg hover:bg-buttonClick">Sem 8</button>
+      </div>
+
+      <div className="bg-surface grid ">
+        {/* Create a table here for all the subjects */}
+      </div>
+    </div>
+  )
+}
+export default SemesterDetails
