2553b5bf225a265aaaabee4b992882bab0cb2303
adarsh <adarshdeshmukh00000@gmail.com>
2026-07-19 03:51:30 +0530

    set up react router in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import LoginPage from './components/loginPage'
 import Sidebar from './components/sidebar'
@@ -13,9 +14,18 @@ function App() {
   
 
   return (
-    <>
-    <Settings></Settings>
-    </>
+    <Router>
+      <Sidebar></Sidebar>
+
+      <Routes>
+        <Route path='/' element={<Dashboard/>}></Route>
+        <Route path='/semester' element={<SemesterDetails/>}></Route>
+        <Route path='/planner' element={<Planner/>}></Route>
+        <Route path='/tasks' element={<Tasks/>}></Route>
+        <Route path='/attendence' element={<Attendence/>}></Route>
+        <Route path='/settings' element={<Settings/>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
