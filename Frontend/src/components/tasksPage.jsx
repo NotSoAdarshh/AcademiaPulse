5f45f3f662cbe757d75943e84d7db0dead7113f0
adarsh <adarshdeshmukh00000@gmail.com>
2026-07-18 23:47:58 +0530

    add sample data to task page

--- Frontend/src/components/tasksPage.jsx
+++ Frontend/src/components/tasksPage.jsx
@@ -1,4 +1,32 @@
 function Tasks(){
+  const tasks =[
+  {
+    "title": "DBMS Assignment 2",
+    "category": "Assignment",
+    "due_status": "Due in 2 days"
+  },
+  {
+    "title": "OS Mid-sem revision",
+    "category": "Revision",
+    "due_status": "Due tomorrow"
+  },
+  {
+    "title": "Daily DSA practice",
+    "category": "Daily",
+    "due_status": "Due today"
+  },
+  {
+    "title": "Discrete Math quiz prep • BACKLOG",
+    "category": "Exam",
+    "due_status": "Overdue by 1 day"
+  },
+  {
+    "title": "Software Eng project report",
+    "category": "Assignment",
+    "due_status": "Due in 5 days"
+  }
+]
+
   return(
     <div>hii</div>
   )
